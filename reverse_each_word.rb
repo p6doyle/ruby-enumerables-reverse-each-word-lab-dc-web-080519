39b44cb273be091d90c93db1a97c1051dcff65ed
Peyton Doyle <p6doyle@gmail.com>
2019-07-21 21:37:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,13 @@
 def reverse_each_word(sentence1)
  string = sentence1.split
+ #takes string and splits it up (for an array later)
  reversed_string = []
+ #builds empty array (to fill later)
  
  string.each do |char|
+  #telling each item of the string to be "processed"
   reversed_string.push(char.reverse)
+  #adds new letters in r
  end
  
  return reversed_string.join(' ')
