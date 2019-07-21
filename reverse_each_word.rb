977731d65e27e3fcda8040b19bb91277257f2911
Peyton Doyle <p6doyle@gmail.com>
2019-07-21 21:29:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,9 +8,6 @@ def reverse_each_word(sentence1)
  
  return reversed_string.join(' ')
  
- string = sentence1.split
- reversed_string = []
- 
  string.collect do |char|
   reversed_string.push(char.reverse)
  end
